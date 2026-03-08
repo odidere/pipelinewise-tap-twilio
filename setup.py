2b9f6e9504d7627c57a81e804df852ab58364669
Oluwashina Aladejubelo <oluwashina.aladejubelo@gopuff.com>
2026-03-08 18:02:34 +0100

    Updated packages

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pipelinewise-tap-twilio',
-      version='1.1.2',
+      version='1.1.3',  # Bumped the version slightly to force a fresh install
       description='Singer.io tap for extracting data from the Twilio API - PipelineWise compatible',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      author='Wise',
-      url='https://github.com/transferwise/pipelinewise-tap-twilio',
+      author='GoPuff',  # Claim your fork!
+      url='https://github.com/gopuff/pipelinewise-tap-twilio',
       classifiers=[
           'License :: OSI Approved :: GNU Affero General Public License v3',
           'Programming Language :: Python :: 3 :: Only'
       ],
       py_modules=['tap_twilio'],
       install_requires=[
-          'requests==2.25.*',
-          'pipelinewise-singer-python==1.*'
+          'requests>=2.25.1',      # Unpinned to survive 3.12 and modern urllib3
+          'singer-python>=5.13.0'  # Swapped the dead fork for the standard Singer library
       ],
       extras_require={
           'test': [
-              'pylint==2.9.*',
-              'pytest==6.2.*'
+              'pylint>=2.9.0',
+              'pytest>=6.2.0'
           ]
       },
-      python_requires='>=3.6',
+      python_requires='>=3.8',     # Let Pants know modern Python is welcome
       entry_points='''
           [console_scripts]
           tap-twilio=tap_twilio:main
